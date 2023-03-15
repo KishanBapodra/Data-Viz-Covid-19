2b367cb19ef0d8eb78c25103f22e92c862c58f9e
kishan <kishanbapodra2002@gmail.com>
2023-03-16 01:58:02 +0400

    Add labels and clean up line-graph

--- line.js
+++ line.js
@@ -1,10 +1,10 @@
 // Dimensions of graph
-const lineMargin = { top: 10, right: 30, bottom: 80, left: 100 };
+const lineMargin = { top: 35, right: 30, bottom: 80, left: 100 };
 const lineWidth = 550 - lineMargin.left - lineMargin.right;
 const lineHeight = 400 - lineMargin.top - lineMargin.bottom;
 
 function lineGraph(data) {
-  d3.select(".lineSVG").remove();
+  d3.select(".line-svg").remove();
   
   // append the svg object to the body of the page using line-viz ID.
   const lineSvg = d3
@@ -12,7 +12,7 @@ function lineGraph(data) {
     .append("svg")
     .attr("width", lineWidth + lineMargin.left + lineMargin.right)
     .attr("height", lineHeight + lineMargin.top + lineMargin.bottom)
-    .attr("class", "lineSVG")
+    .attr("class", "line-svg")
     .attr("stroke", "#98C1D9")
     .append("g")
     .attr("transform", `translate(${lineMargin.left},${lineMargin.top})`);
@@ -31,26 +31,42 @@ function lineGraph(data) {
     .domain([0, d3.max(data, (d) => +d.total_cases+d.total_cases*0.1)])
     .range([lineHeight, 0]);
 
+  // add axis and label
+  // x-axis
   lineSvg
     .append("g")
     .attr("transform", `translate(0, ${lineHeight})`)
-    .call(d3.axisBottom(x).ticks(5));
+    .call(d3.axisBottom(x).ticks(5))
+    .call(g => g.append("text")
+        .attr("x", lineWidth)
+        .attr("y", lineMargin.bottom - 50)
+        .attr("fill", "#98C1D9")
+        .attr("text-anchor", "end")
+        .text("Income per capita (dollars) →"));
 
-  lineSvg.append("g").call(d3.axisLeft(y));
+  // y-axis
+  lineSvg.append("g")
+    .call(d3.axisLeft(y))
+    .call(g => g.append("text")
+    .attr("x", -lineMargin.left)
+    .attr("y", -15)
+    .attr("fill", "#98C1D9")
+    .attr("text-anchor", "start")
+    .text("↑ Total Covid-19 Cases"));
 
+  // get data and create line
   const line = lineSvg
     .selectAll(".line")
     .data(d3.group(data, d => d.location))
     .join("path")
     .attr("fill", "none")
     .attr("stroke", "steelblue")
-    .attr("stroke-width", 1)
     .attr("d", d => d3.line()
       .x(d => x(d3.timeParse("%Y-%m-%d")(d.date)))
       .y(d => y(+d.total_cases))
       (d[1])
     )
-    .transition()
+    .transition() // animate line
     .duration(1500) // animation duration
     .ease(d3.easeSin) 
     .attrTween("stroke-dasharray", function() {
@@ -62,7 +78,7 @@ function lineGraph(data) {
 
   lineSvg.append('text')
     .attr("x", lineWidth/2)
-    .attr("y", lineHeight + 50)
+    .attr("y", lineHeight + 60)
     .attr("text-anchor", "middle")
     .style("font-size", "0.75em")
     .style("fill", "black") 
